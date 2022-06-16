1c425563f8df3e6d91c5b0ec59cb6a9bd75be2dd
heyu <1395294694@qq.com>
2022-06-16 21:19:33 +0800

    fix: 解决组件文档中组件名未被替换的bug

--- generator/markdown.js
+++ generator/markdown.js
@@ -3,7 +3,7 @@ const path = require('path')
 const fs = require('fs-extra')
 
 function generatorMarkdown(projectDir, inputObj) {
-    const docPath = path.resolve(projectDir, 'README.md')
+    const docPath = path.resolve(projectDir, 'src' ,'README.md')
     const content = fs.readFileSync(docPath, 'utf-8')
     const template = Handlebars.compile(content);
     const fileContent = template(inputObj)
